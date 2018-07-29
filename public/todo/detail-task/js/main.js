c06ff8fc24dea0d1dfb8cfc4108a056207caa270
Tran Dinh Khang <khangtd.09@gmail.com>
2018-07-30 03:22:58 +0700

    build and commit

--- public/todo/detail-task/js/main.js
+++ public/todo/detail-task/js/main.js
@@ -81,6 +81,18 @@ function formatDateToTimeString(date){
     return (hh + ":" + mm + ":" + ss);
 }
 
+function getDateStandard(date){
+     // 01, 02, 03, ... 29, 30, 31
+    var dd = (date.getDate() < 10 ? '0' : '') + date.getDate();
+    // 01, 02, 03, ... 10, 11, 12
+    var MM = ((date.getMonth() + 1) < 10 ? '0' : '') + (date.getMonth() + 1);
+    // 1970, 1971, ... 2015, 2016, ...
+    var yyyy = date.getFullYear();
+
+    // create the format you want
+    return (yyyy + "-" + MM + "-" + dd);
+}
+
 
 /* todo app's permission */
 function isManagerOrAdmin(userActor, userActorSubordinateList, userTargetID){
